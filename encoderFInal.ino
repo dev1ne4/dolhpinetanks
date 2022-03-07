cac4e8e1210e0c6c34b75b7b39c4a9087e33ed93
dev1ne4 <99689200+dev1ne4@users.noreply.github.com>
2022-03-07 14:29:53 -0700

    Add files via upload

--- encoderFInal.ino
+++ encoderFInal.ino
@@ -17,6 +17,8 @@ static int clkPin2 = 3, dtPin2 = 9;
 int mech2CurrentState = LOW, mech2LastState = mech2CurrentState;
 
 volatile int aVal = 0, bVal = 0, TRAVEL1 = 0, TRAVEL2 = 0; //TRAVEL is position, mentioned in section 3.3, but not required for demonstration
+volatile float angleL = 0, angleR = 0;
+float angle = 0;
 
 void setup() {
   pinMode(clkPin1, INPUT_PULLUP);
@@ -24,11 +26,13 @@ void setup() {
 
   pinMode(clkPin2, INPUT_PULLUP);
   pinMode(dtPin2, INPUT_PULLUP);
+
+  pinMode(13, INPUT); //button for reset
   
   attachInterrupt(0, mechISR1, RISING);  //enables ISR for pin 2, or clkPin
   attachInterrupt(1, mechISR2, RISING);
 
-  Serial.begin(250000); 
+  Serial.begin(19200); 
 }
 
 void mechISR1(){ //clkPin ISR
@@ -37,11 +41,11 @@ void mechISR1(){ //clkPin ISR
     aVal = digitalRead(clkPin1); bVal = digitalRead(dtPin1);
     if(aVal == bVal){
       //Serial.println("CCW");  //if pins are equal, moved CCW
-      TRAVEL1+=4;
+      TRAVEL1-=4;
     }
     else {
       //Serial.println("CW"); //if pins are different, moved CW
-      TRAVEL1-=4;
+      TRAVEL1+=4;
     }
     
     sei();
@@ -53,11 +57,11 @@ void mechISR2(){ //clkPin ISR
     aVal = digitalRead(clkPin2); bVal = digitalRead(dtPin2);
     if(aVal == bVal){
       //Serial.println("CW");  //if pins are equal, moved CCW
-      TRAVEL2-=4;
+      TRAVEL2+=4;
     }
     else {
       //Serial.println("CCW"); //if pins are different, moved CW
-      TRAVEL2+=4;
+      TRAVEL2-=4;
     }
     
     sei();
@@ -65,5 +69,8 @@ void mechISR2(){ //clkPin ISR
   
 
 void loop() {
-      Serial.print(TRAVEL1); Serial.print("\t"); Serial.println(TRAVEL2);
+      Serial.print(TRAVEL1); Serial.print("\t"); Serial.print(TRAVEL2); Serial.print("\t"); Serial.println("EROR 610");
+      /*if(digitalRead(13) == 1){
+        angleL = 0; angleR = 0; TRAVEL1 = 0; TRAVEL2=0;
+        }*/
 }
